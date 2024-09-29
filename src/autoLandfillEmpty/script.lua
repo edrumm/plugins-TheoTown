f0de9acb11668070cd4c281d86f2c6bf063809a4
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2024-09-29 13:44:56 +0100

    basic script implementations

--- src/autoLandfillEmpty/script.lua
+++ src/autoLandfillEmpty/script.lua
@@ -1,7 +1,21 @@
-function script:init()
+local settings
 
+function script:init()
+    settings = Util.optStorage(TheoTown.getStorage(), self:getDraft():getId()..":settings")
+    -- TODO
+end
+
+function script:settings()
+    -- TODO
 end
 
 function script:update()
+    local.landfills = City.getBuildings("landfill")
 
+    for _, lf in ipairs(landfills) do
+        if lf.isFull() and City.getMoney() >= lf.emptyCost then
+            lf.empty()
+            City.addMoney(-lf.emptyCost)
+        end
+    end
 end
--- src/autoWinterCycle/script.lua
+++ src/autoWinterCycle/script.lua
@@ -1,14 +1,20 @@
 local settings
 
 function script:init()
-    settings = Util.optStorage(TheoTown.getStorage(), self:getDraft():getId()..':settings')
+    settings = Util.optStorage(TheoTown.getStorage(), self:getDraft():getId()..":settings")
     -- TODO
 end
 
 function script:settings()
-
+    -- TODO
 end
 
 function script:update()
+    local month = City.getDate().month
 
+    if month >= 11 or month <= 2 then
+        game.setWinter(true)
+    else
+        game.setWinter(false)
+    end
 end
